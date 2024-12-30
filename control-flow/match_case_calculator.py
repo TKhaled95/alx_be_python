4fe35a6be9012eb5727c5a927ad352a385a64cff
tkhaled <tkhaled702@gmail.com>
2024-12-30 22:02:10 +0400

    modifing match_case_calculator.py file

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -15,10 +15,10 @@ match operation:
         result = num1 * num2
         print(f"The result is {result}")
     case "/":
-        if result !=0:
+        if num2 != 0:
             result = num1 / num2
             print(f"The result is {result}")
         else:
              print("Cannot divide by zero.")
     case _:
-         print("Cannot divide by zero.")
+         print("Invalid operation selected.")
