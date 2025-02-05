dbf6158106850ea69c1a75c28d78c1c0a5628202
tkhaled <tkhaled702@gmail.com>
2025-02-05 19:26:23 +0400

    modefing

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -1,6 +1,7 @@
 # Define global conversion factors with the exact format
-FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
 CELSIUS_TO_FAHRENHEIT_FACTOR = 9 / 5
+FAHRENHEIT_TO_CELSIUS_FACTOR = 5 / 9
+
 
 # Function to convert Fahrenheit to Celsius
 def convert_to_celsius(fahrenheit):
